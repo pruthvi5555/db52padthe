495c7611ebd72064eeb39e612f35833da20bf71d
pruthvi5555 <77819499+pruthvi5555@users.noreply.github.com>
2021-04-07 15:54:41 -0500

    implementation

--- controllers/fan.js
+++ controllers/fan.js
@@ -16,9 +16,24 @@ exports.fan_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: fan detail: ' + req.params.id);
 };
 // Handle fan create on POST.
-exports.fan_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: fan create POST');
-};
+exports.fan_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new fan();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costumetype":"goat", "cost":12, "size":"large"}
+    document.CompanyName = req.body.CompanyName;
+    document.fanType = req.body.fanType;
+    document.price = req.body.price;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
 // Handle fan delete form on DELETE.
 exports.fan_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: fan delete DELETE ' + req.params.id);
@@ -27,3 +42,14 @@ res.send('NOT IMPLEMENTED: fan delete DELETE ' + req.params.id);
 exports.fan_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: fan update PUT' + req.params.id);
 };
+// VIEWS
+// Handle a show all view
+exports.fan_view_all_Page = async function(req, res) {
+    try{
+    thefans = await fan.find();
+    res.render('fans', { title: 'Fan Search Results', results: thefans });
+    }
+    catch(err){
+    res.error(500,`{"error": ${err}}`);
+    }
+    };
--- routes/fans.js
+++ routes/fans.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const fan_controlers= require('../controllers/fan');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('fans', { title: 'Search Results fans' });
-});
+router.get('/', fan_controlers.fan_view_all_Page ); 
 
 module.exports = router;
